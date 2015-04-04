e9247858b3f0fdb8ca31adf977f946f09756f399
Felipe Hanashiro <felipe.hanashiro@exla.com.br>
2015-04-04 18:52:08 -0300

    anuncio bugs fixed

--- src/main/java/br/com/marketchase/models/repositories/AnuncioRepository.java
+++ src/main/java/br/com/marketchase/models/repositories/AnuncioRepository.java
@@ -15,7 +15,7 @@ public interface AnuncioRepository extends CrudRepository<Anuncio, Long> {
 	@Query("SELECT an FROM Anuncio an WHERE an.ativo = 'true' ORDER BY an.dataPostagem DESC")
 	public List<Anuncio> findAll();
 	
-	@Query("SELECT an FROM Anuncio an INNER JOIN an.loja as loj WHERE loj.codigo = ?")
+	@Query("SELECT an FROM Anuncio an INNER JOIN an.loja as loj WHERE loj.codigo = ? and an.ativo = 'true'")
 	public List<Anuncio> findByLoja(Long id);
 	
 	@Query("SELECT an FROM Anuncio an WHERE an.categoria = ?")
